9bb39fd23f4cf1437b20a23b1a409ddc0b72cd2d
jiaweikhong <khongjw@gmail.com>
2019-02-24 00:19:46 +0800

    Basic Firebase login system complete

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
 app = Flask(__name__)
 
-@app.route("/")
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate('term-5-esc-scheduler-firebase-adminsdk-cfadg-3d9e06988a.json')
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://term-5-esc-scheduler.firebaseio.com/'
+})
+# Set firebase database reference
+ref = db.reference('users')
+
+# Route for handling the login page logic
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    error = None
+    if request.method == 'POST':
+        # Check if username match password
+        check_username = request.form['username']
+        check_password = request.form['password']
+
+        if (ref.child(check_username).get() != check_password):
+            error = 'Invalid Credentials. Please try again.'
+
+        else:
+            return redirect(url_for('dashboard'))
+    return render_template('index.html', error=error)
 
 @app.route("/dashboard")
 def dashboard():
