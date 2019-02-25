7697f29d28eda766119d14655305eed0dc4ce1be
jiaweikhong <khongjw@gmail.com>
2019-02-25 21:02:06 +0800

    Set up login for admin and instructor. Can view schedule.

--- main.py
+++ main.py
@@ -6,15 +6,20 @@ from firebase_admin import db
 app = Flask(__name__)
 
 # Fetch the service account key JSON file contents
-cred = credentials.Certificate('term-5-esc-scheduler-firebase-adminsdk-cfadg-3d9e06988a.json')
+cred = credentials.Certificate(
+    'term-5-esc-scheduler-firebase-adminsdk-cfadg-3d9e06988a.json')
 # Initialize the app with a service account, granting admin privileges
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://term-5-esc-scheduler.firebaseio.com/'
 })
 # Set firebase database reference
-ref = db.reference('users')
+ref = db.reference('users')     # used for testing
+adminRef = db.reference('admins')
+instructorRef = db.reference('instructors')
 
 # Route for handling the login page logic
+
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
     error = None
@@ -30,9 +35,77 @@ def index():
             return redirect(url_for('dashboard'))
     return render_template('index.html', error=error)
 
+
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
+
+# Combining with Si Wei
+
+
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+# Instructor:
+
+
+@app.route("/instructorhome", methods=['GET', 'POST'])
+def instructorhome():
+    error = None
+    if request.method == 'POST':
+        # Check if username match password
+        check_username = request.form['username']
+        check_password = request.form['password']
+
+        if (instructorRef.child(check_username).get() != check_password):
+            error = 'Invalid Credentials. Please try again.'
+
+        else:
+            return redirect(url_for('instructorwelcome'))
+    return render_template('instructorhome.html', error=error)
+
+
+@app.route("/instructorwelcome")
+def instructorwelcome():
+    return render_template('instructorwelcome.html')
+
+# Admin:
+
+
+@app.route("/adminhome", methods=['GET', 'POST'])
+def adminhome():
+    error = None
+    if request.method == 'POST':
+        # Check if username match password
+        check_username = request.form['username']
+        check_password = request.form['password']
+
+        if (adminRef.child(check_username).get() != check_password):
+            error = 'Invalid Credentials. Please try again.'
+
+        else:
+            return redirect(url_for('adminwelcome'))
+    return render_template('adminhome.html', error=error)
+
+
+@app.route("/adminwelcome", methods=['GET', 'POST'])
+def adminwelcome():
+    if request.method == 'POST':
+        if "createSchedule" in request.form:
+            return redirect(url_for('courseSchedule'))
+    return render_template('adminwelcome.html')
+
+
+@app.route("/schedulecreation")
+def schedulecreation():
+    return render_template('schedulecreation.html')
+
+
+@app.route("/courseSchedule")
+def courseSchedule():
+    return render_template('courseSchedule.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
