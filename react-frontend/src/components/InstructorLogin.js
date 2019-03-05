99b59b81c79b640838a4839b5b08b254ab46c224
jiaweikhong <khongjw@gmail.com>
2019-03-05 19:03:14 +0800

    Update InstructorLogin.js

--- react-frontend/src/components/InstructorLogin.js
+++ react-frontend/src/components/InstructorLogin.js
@@ -1,29 +1,19 @@
 import React from 'react';
-
+import { NavLink } from 'react-router-dom'; 
 
 const InstructorLogin = () => {
-
 	return (
-
 		<html>
-			{/* <head>
-				<title>Instructor Login</title>
-			</head> */}
-
 			<body>
-
 				<h1>Login as Instructor</h1>
 
 				<form action="" method="post">
 					<label for="username">Username: </label>
 					<input type="text" name="username" required="required"></input><br />
-
 					<label for="password">Password:	</label>
 					<input type="password" name="password" required="required"></input><br />
-
 					<input type="submit" name="instructorsubmit" value="Submit"></input>
-
-					<p><a href="instructorwelcome">bypass</a></p>
+					<p><NavLink to="/instructorwelcome">bypass</NavLink></p>
 				</form>
 			</body>
 		</html>
