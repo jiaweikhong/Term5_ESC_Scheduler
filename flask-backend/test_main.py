dc5b64c051a5493659767fd00cadfb305645df91
Khong Jia Wei <43800932+jiaweikhong@users.noreply.github.com>
2019-03-24 17:55:58 +0800

    updated a few more test cases

--- flask-backend/test_main.py
+++ flask-backend/test_main.py
@@ -3,16 +3,22 @@ import main  # import main.py
 
 class TestMain(unittest.TestCase):
     def test_admin_login(self):
-        self.assertEqual(main.check_admin_login("tom", "tommy"), 0)
-        self.assertEqual(main.check_admin_login("tom", "tom"), 1)
+        self.assertEqual(main.check_admin_login("tom", "tommy"), 0) # correct username, wrong pw, should fail.
+        self.assertEqual(main.check_admin_login("karen", "karen"), 0)   # instructor credentials should fail.
+        self.assertEqual(main.check_admin_login("bob", "bob"), 0) # planner credentials should fail.
+        self.assertEqual(main.check_admin_login("tom", "tom"), 1)   # correct username & pw, should pass.
 
     def test_instructor_login(self):
-        self.assertEqual(main.check_instructor_login("karen", "wrongpw"), 0)
-        self.assertEqual(main.check_instructor_login("karen", "karen"), 1)
+        self.assertEqual(main.check_instructor_login("karen", "wrongpw"), 0)    # correct username, wrong pw, should fail.
+        self.assertEqual(main.check_instructor_login("tom", "tom"), 0)   # admin credentials should fail.
+        self.assertEqual(main.check_instructor_login("bob", "bob"), 0) # planner credentials should fail.
+        self.assertEqual(main.check_instructor_login("karen", "karen"), 1)  # correct username & pw, should pass.
 
     def test_planner_login(self):
-        self.assertEqual(main.check_planner_login("lolo", "lala"), 0)
-        self.assertEqual(main.check_planner_login("bob", "bob"), 1)
+        self.assertEqual(main.check_planner_login("bob", "lala"), 0)   # correct username, wrong pw, should fail.
+        self.assertEqual(main.check_planner_login("tom", "tom"), 0)   # admin credentials should fail.
+        self.assertEqual(main.check_planner_login("karen", "karen"), 0)  # instructor credentials should fail.
+        self.assertEqual(main.check_planner_login("bob", "bob"), 1) # correct username & pw, should pass
 
 if __name__ == '__main__':      # allows you to run using "python test_main.py" in terminal, or inside code editor
     unittest.main(exit=False)
