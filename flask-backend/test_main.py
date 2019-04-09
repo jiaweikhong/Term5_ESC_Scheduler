c6341e3baeb8f80d8a6685ff9f9e530f0debf5be
jiaweikhong <khongjw@gmail.com>
2019-04-09 10:07:19 +0800

    updated login test and epd course code

--- flask-backend/test_main.py
+++ flask-backend/test_main.py
@@ -3,16 +3,16 @@ import main  # import main.py
 
 class TestMain(unittest.TestCase):
     def test_admin_login(self):
-        self.assertEqual(main.check_admin_login("tom", "tommy"), 0) # correct username, wrong pw, should fail.
-        self.assertEqual(main.check_admin_login("karen", "karen"), 0)   # instructor credentials should fail.
+        self.assertEqual(main.check_admin_login("ISTDadmin", "haha"), 0) # correct username, wrong pw, should fail.
+        self.assertEqual(main.check_admin_login("Sudipta", "sudipta"), 0)   # instructor credentials should fail.
         self.assertEqual(main.check_admin_login("bob", "bob"), 0) # planner credentials should fail.
-        self.assertEqual(main.check_admin_login("tom", "tom"), 1)   # correct username & pw, should pass.
+        self.assertEqual(main.check_admin_login("ISTDadmin", "pw"), 1)   # correct username & pw, should pass.
 
     def test_instructor_login(self):
         self.assertEqual(main.check_instructor_login("karen", "wrongpw"), 0)    # correct username, wrong pw, should fail.
         self.assertEqual(main.check_instructor_login("tom", "tom"), 0)   # admin credentials should fail.
         self.assertEqual(main.check_instructor_login("bob", "bob"), 0) # planner credentials should fail.
-        self.assertEqual(main.check_instructor_login("karen", "karen"), 1)  # correct username & pw, should pass.
+        self.assertEqual(main.check_instructor_login("Sudipta", "sudipta"), 1)  # correct username & pw, should pass.
 
     def test_planner_login(self):
         self.assertEqual(main.check_planner_login("bob", "lala"), 0)   # correct username, wrong pw, should fail.
